ff5c47b0de019c781ad7639b3accf3f10f0e60e0
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-07 02:27:07 +0600

    KS-1-2 print variables.

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,4 +1,14 @@
 package org.example.lesson_1
 
-class lesson1_task2 {
+fun main() {
+    val orderCounts: Int = 75
+    val gratitudeText: String = "Благодарим за покупку!"
+    println("orderCounts: $orderCounts")
+    println("gratitudeText: $gratitudeText")
+
+    var employeesCount: Int = 2000
+//    println("employeesCount: $employeesCount")
+
+    employeesCount -= 1
+    println("employeesCount: $employeesCount")
 }
