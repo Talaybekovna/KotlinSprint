1ab0008ea709e372e2a07aa390c561eece43d7a2
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-07 03:05:47 +0600

    KS-1-5 convert seconds to hours and minutes.

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,4 +1,10 @@
 package org.example.lesson_1
 
-class lesson1_task5 {
+fun main() {
+    val totalSeconds = 5409
+    val hours = totalSeconds / 60 / 60
+    val minutes = totalSeconds / 60 - hours * 60
+    val seconds = totalSeconds - (hours * 60 * 60 + minutes * 60)
+    print("0$hours:$minutes:0$seconds")
+
 }
