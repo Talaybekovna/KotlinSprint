d6e148fc60d4cf8dd0c9d0eb553e1bbbffde7e10
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-07 02:51:36 +0600

    KS-1-4 specify min type .

--- src/main/kotlin/lesson_1/lesson1_task4.kt
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -1,4 +1,13 @@
 package org.example.lesson_1
 
-class lesson1_task4 {
+fun main() {
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val aged = 27
+    val partOfDay: Float = 0.075F
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+    print("Расстояние $length\nВозраст - $age\nЧасть дня – $partOfDay" +
+    "\nСекунды - $seconds\nЧасть года – $partOfYear\nАпогей орбиты – $apogee")
 }
