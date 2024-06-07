7cb5ee467c20b57c2e4bcc997f9097f370165f82
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-08 02:23:26 +0600

    KS-2-2 calculate average salary payments.

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,3 +1,17 @@
 package org.example.lesson_2
 
-fun main() {}
+fun main() {
+    val employeesCount = 50
+    val employeesSalary = 30_000
+    val traineesCount = 20
+    val traineesSalary = 20_000
+
+    val employeesPayments = employeesCount * employeesSalary
+    val traineesPayments = traineesCount * traineesSalary
+    val totalPayments = employeesPayments + traineesPayments
+    val averageSalary = (employeesPayments + traineesPayments) / (employeesCount + traineesCount)
+
+    println("Расходы на выплату зарплаты постоянных сотрудников: $employeesPayments")
+    println("Общие расходы по ЗП после прихода стажеров: $totalPayments")
+    println("Средня ЗП одного сотрудника после устройства стажеров: $averageSalary")
+}
