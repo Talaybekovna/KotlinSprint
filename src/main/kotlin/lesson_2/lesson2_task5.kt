157592e8acb1f0d571116d1a1f63a01d4f2713d3
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-08 23:43:59 +0600

    KS-2-5 calculate deposit.

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,3 +1,13 @@
 package org.example.lesson_2
 
-fun main() {}
+import kotlin.math.pow
+
+fun main() {
+    val deposit = 70_000
+    val percentage = 16.7
+    val term = 20
+
+    val result = deposit * (1 + percentage / 100).pow(term)
+    val totalDeposit = String.format("%.3f", result)
+    println(totalDeposit)
+}
