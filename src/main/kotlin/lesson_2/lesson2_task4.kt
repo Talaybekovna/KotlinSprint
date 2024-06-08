737cb919ff90864da7e00f32d058d2d317519ae5
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-08 23:10:49 +0600

    KS-2-4 calculate baff.

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,3 +1,13 @@
 package org.example.lesson_2
 
-fun main() {}
+fun main() {
+    val crystals: Int = 7
+    val ironOres: Int = 11
+    val baff = 0.2
+
+    val baffForCrystals = (crystals * baff).toInt()
+    val baffForIronOres = (ironOres * baff).toInt()
+
+    println("baffForCrystals: $baffForCrystals")
+    println("baffForIronOres: $baffForIronOres")
+}
