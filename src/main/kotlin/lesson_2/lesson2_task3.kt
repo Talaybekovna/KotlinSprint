397424152c801476277bf885d85e841cb69d029d
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-08 22:02:45 +0600

    KS-2-2 calculate arrival time.

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,3 +1,19 @@
 package org.example.lesson_2
 
-fun main() {}
+fun main() {
+    val departureTimeInHours = 9
+    val departureTimeInMinutes = 39
+    val totalTravelTimeInMinutes = 457
+
+    val travelTimeInHours = totalTravelTimeInMinutes / 60
+    val travelTimeInMinutes = totalTravelTimeInMinutes % 60
+
+    val totalArrivalTimeInMinutes =
+        (departureTimeInHours + travelTimeInHours) * 60 + (departureTimeInMinutes + travelTimeInMinutes)
+
+    val arrivalTimeInHours = totalArrivalTimeInMinutes / 60
+    val arrivalTimeInMinutes = totalArrivalTimeInMinutes % 60
+
+    val arrivalTime = "$arrivalTimeInHours:$arrivalTimeInMinutes"
+    println(arrivalTime)
+}
