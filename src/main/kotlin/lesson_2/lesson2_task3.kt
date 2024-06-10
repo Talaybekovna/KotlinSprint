a173734e4a93b74f24efeac3ff97b0536713f60b
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-10 18:14:01 +0600

    KS-2-3 calculate arrival time.

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,19 +1,19 @@
 package org.example.lesson_2
 
+const val TIME_UNITS = 60
 fun main() {
     val departureTimeInHours = 9
     val departureTimeInMinutes = 39
     val totalTravelTimeInMinutes = 457
 
-    val travelTimeInHours = totalTravelTimeInMinutes / 60
-    val travelTimeInMinutes = totalTravelTimeInMinutes % 60
+    val travelTimeInHours = totalTravelTimeInMinutes / TIME_UNITS
+    val travelTimeInMinutes = totalTravelTimeInMinutes % TIME_UNITS
 
     val totalArrivalTimeInMinutes =
-        (departureTimeInHours + travelTimeInHours) * 60 + (departureTimeInMinutes + travelTimeInMinutes)
+        (departureTimeInHours + travelTimeInHours) * TIME_UNITS + (departureTimeInMinutes + travelTimeInMinutes)
 
-    val arrivalTimeInHours = totalArrivalTimeInMinutes / 60
-    val arrivalTimeInMinutes = totalArrivalTimeInMinutes % 60
+    val arrivalTimeInHours = totalArrivalTimeInMinutes / TIME_UNITS
+    val arrivalTimeInMinutes = totalArrivalTimeInMinutes % TIME_UNITS
 
-    val arrivalTime = "$arrivalTimeInHours:$arrivalTimeInMinutes"
-    println(arrivalTime)
+    println("$arrivalTimeInHours:$arrivalTimeInMinutes")
 }
