9f96d770a7423492a59180ccb1c787cafb2d04f9
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-11 01:15:38 +0600

    KS-2-5 update variable.

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -2,12 +2,13 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
+const val PERCENT = 100
 fun main() {
     val deposit = 70_000
     val percentage = 16.7
     val term = 20
 
-    val result = deposit * (1 + percentage / 100).pow(term)
+    val result = deposit * (1 + percentage / PERCENT).pow(term)
     val totalDeposit = String.format("%.3f", result)
     println(totalDeposit)
 }
