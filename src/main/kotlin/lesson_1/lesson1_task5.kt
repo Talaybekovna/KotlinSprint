069bacc4be66c17d4bb958d3cda292a29e826238
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-10 17:59:59 +0600

    KS-1-5 correct digit format and variables.

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,10 +1,13 @@
 package org.example.lesson_1
 
+const val TIME_UNITS = 60
 fun main() {
     val totalSeconds = 5409
-    val hours = totalSeconds / 60 / 60
-    val minutes = totalSeconds / 60 - hours * 60
-    val seconds = totalSeconds - (hours * 60 * 60 + minutes * 60)
-    print("0$hours:$minutes:0$seconds")
-
+    val hours = totalSeconds / TIME_UNITS / TIME_UNITS
+    val minutes = totalSeconds / TIME_UNITS - hours * TIME_UNITS
+    val seconds = totalSeconds - (hours * TIME_UNITS * TIME_UNITS + minutes * TIME_UNITS)
+    val formattedHours = String.format("%02d", hours)
+    val formattedMinutes = String.format("%02d", minutes)
+    val formattedSeconds = String.format("%02d", seconds)
+    print("$formattedHours:$formattedMinutes:$formattedSeconds")
 }
