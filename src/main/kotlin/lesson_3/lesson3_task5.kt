89fbd4d55bfe507ce8279f53e7bc3d45305d994d
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-13 02:44:50 +0600

    KS-3-5 use split instead of substring.

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -1,17 +1,13 @@
 package org.example.lesson_3
 
 fun main() {
-    val stepFrom: String
-    val stepTo: String
-    val stepNumber: Int
     val response = "D2-D4;0"
 
-    stepFrom = response.substring(0, 2)
-    stepTo = response.substring(3, 5)
-    stepNumber = response.substring(6).toInt()
+    val responseList = response.split("-", ";")
+
     println("""
-        $stepFrom
-        $stepTo
-        $stepNumber
+        ${responseList[0]}
+        ${responseList[1]}
+        ${responseList[2]}
     """.trimIndent())
 }
