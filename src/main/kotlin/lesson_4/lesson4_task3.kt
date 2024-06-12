2aaff37f36945f5a9791dde6d8aa9b2440c2bc14
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-13 03:30:05 +0600

    KS-4-3 compare values.

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,3 +1,16 @@
 package org.example.lesson_4
 
-fun main() {}
+const val SUNNY = true
+const val OPEN_TENT = true
+const val AIR_HUMIDITY_20 = 20
+const val WINTER = "Зима"
+fun main() {
+    val sunnyToday = true
+    val openTentNow = true
+    val airHumidity = 20
+    val season = "Зима"
+
+    println("Благоприятные ли условия сейчас для роста бобовых? " +
+            "${(sunnyToday == SUNNY) && (openTentNow == OPEN_TENT) && 
+                    (airHumidity == AIR_HUMIDITY_20) && (season != WINTER)}")
+}
