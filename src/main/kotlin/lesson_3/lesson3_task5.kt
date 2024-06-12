6087c1617b46d4a0726379bd885ce6156ae81f9a
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-13 01:43:31 +0600

    KS-3-5 parse server response.

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -1,3 +1,17 @@
 package org.example.lesson_3
 
-fun main() {}
+fun main() {
+    val stepFrom: String
+    val stepTo: String
+    val stepNumber: Int
+    val response = "D2-D4;0"
+
+    stepFrom = response.substring(0, 2)
+    stepTo = response.substring(3, 5)
+    stepNumber = response.substring(6).toInt()
+    println("""
+        $stepFrom
+        $stepTo
+        $stepNumber
+    """.trimIndent())
+}
