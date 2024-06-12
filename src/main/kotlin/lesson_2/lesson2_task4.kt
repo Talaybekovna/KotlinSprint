36795199e96a0a4f9b1640f2f7d5eb82a26c1ae3
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-12 17:53:34 +0600

    KS-2-4 fix variables.

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_2
 
-const val BAFF = 0.2
+const val PERCENT = 100
 fun main() {
     val crystals = 7
     val ironOres = 11
+    val baff = 20
 
-    val baffForCrystals = (crystals * BAFF).toInt()
-    val baffForIronOres = (ironOres * BAFF).toInt()
+    val baffForCrystals = crystals * baff / PERCENT
+    val baffForIronOres = ironOres * baff / PERCENT
 
     println("baffForCrystals: $baffForCrystals")
     println("baffForIronOres: $baffForIronOres")
