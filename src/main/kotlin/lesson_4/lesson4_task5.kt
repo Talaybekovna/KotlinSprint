a77402c0f465c7f1e71d442ac83076b1c4f291d2
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-14 03:04:19 +0600

    KS-4-5 define permission for a ship.

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,3 +1,26 @@
 package org.example.lesson_4
 
-fun main() {}
+const val HAS_DAMAGES = false
+const val MIN_CREW_COUNT = 55
+const val MAX_CREW_COUNT = 70
+const val MIN_BOXES_OF_FOOD = 50
+const val IS_WEATHER_GOOD = true
+fun main() {
+    println("наличие повреждений корпуса:")
+    val hasDamages = readln().toBoolean()
+
+    println("текущий состав экипажа:")
+    val crewCount = readln().toInt()
+
+    println("количество ящиков с провизией на борту:")
+    val boxesOfFood = readln().toInt()
+
+    println("благоприятность метеоусловий:")
+    val isWeatherGood = readln().toBoolean()
+
+    val result = (hasDamages == HAS_DAMAGES) && (crewCount in MIN_CREW_COUNT..MAX_CREW_COUNT) &&
+            (boxesOfFood > MIN_BOXES_OF_FOOD) && (isWeatherGood == IS_WEATHER_GOOD || isWeatherGood != IS_WEATHER_GOOD) ||
+            (hasDamages != HAS_DAMAGES) && (crewCount == MAX_CREW_COUNT) &&
+            (boxesOfFood >= MIN_BOXES_OF_FOOD) && (isWeatherGood == IS_WEATHER_GOOD)
+    println(result)
+}
