762bb2c4955cdd5550bd44d0c7eee0aa3f927e7f
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-14 03:31:59 +0600

    KS-5-2 calculate user age for permission.

--- src/main/kotlin/lesson_5/lesson5_task2.kt
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -1,3 +1,12 @@
 package org.example.lesson_5
 
-fun main() {}
+const val AGE_OF_MAJORITY = 18
+const val PRESENT_YEAR = 2024
+fun main() {
+    println("Введите год вашего рождения:")
+    val userBornYear = readln().toInt()
+
+    val userAge = PRESENT_YEAR - userBornYear
+
+    if (userAge >= AGE_OF_MAJORITY)  println("Показать экран со скрытым контентом")
+}
