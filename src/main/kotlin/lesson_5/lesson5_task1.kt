0ec72e3b972c46429b557674d348d63febe460dc
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-14 03:24:23 +0600

    KS-5-1 setup login.

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,3 +1,10 @@
 package org.example.lesson_5
 
-fun main() {}
+const val CORRECT_ANSWER = 10
+fun main() {
+    println("Для входа в приложение решите пример: 3 + 7 = ?")
+    val answer = readln().toInt()
+
+    if (answer == CORRECT_ANSWER) println("Добро пожаловать!")
+    else println("Доступ запрещен.")
+}
