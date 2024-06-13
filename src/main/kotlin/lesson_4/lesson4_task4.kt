ff5f52b67945238a9ddb28ee58ae0a1ac9a692a0
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-14 02:31:49 +0600

    KS-4-4 define days for exercises.

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,3 +1,13 @@
 package org.example.lesson_4
 
-fun main() {}
+const val EVEN = true
+fun main() {
+    val day = 5
+
+    println("""
+        Упражнения для рук: ${(day % 2 == 0) != EVEN}
+        Упражнения для ног: ${(day % 2 == 0) == EVEN}
+        Упражнения для спины: ${(day % 2 == 0) == EVEN}
+        Упражнения для пресса: ${(day % 2 == 0) != EVEN}
+    """.trimIndent())
+}
