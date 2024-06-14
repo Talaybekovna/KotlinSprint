9c32c16dd5189f69780f455b18c67263e4f9750f
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-15 03:26:44 +0600

    KS-5-1 update variable.

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,8 +1,10 @@
 package org.example.lesson_5
 
-const val CORRECT_ANSWER = 10
+const val FIRST_NUMBER = 3
+const val SECOND_NUMBER = 7
+const val CORRECT_ANSWER = FIRST_NUMBER + SECOND_NUMBER
 fun main() {
-    println("Для входа в приложение решите пример: 3 + 7 = ?")
+    println("Для входа в приложение решите пример: ${FIRST_NUMBER + SECOND_NUMBER} = ?")
     val answer = readln().toInt()
 
     if (answer == CORRECT_ANSWER) println("Добро пожаловать!")
