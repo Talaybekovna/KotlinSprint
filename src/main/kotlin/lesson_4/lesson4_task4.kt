8d1fa31e3053bf383b257bcee4f0a9ec63b5482b
Kunduz <talaybekovna.kunduz@gmail.com>
2024-06-16 00:57:34 +0600

    KS-4-4 correct variables.

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,13 +1,19 @@
 package org.example.lesson_4
 
-const val EVEN = true
 fun main() {
     val day = 5
 
+    val isEven = day % 2 == 0
+
+    val handsExercises = !isEven
+    val legsExercises = isEven
+    val backExercises = isEven
+    val absExercises = !isEven
+
     println("""
-        Упражнения для рук:    ${(day % 2 == 0) != EVEN}
-        Упражнения для ног:    ${(day % 2 == 0) == EVEN}
-        Упражнения для спины:  ${(day % 2 == 0) == EVEN}
-        Упражнения для пресса: ${(day % 2 == 0) != EVEN}
+        Упражнения для рук:    $handsExercises
+        Упражнения для ног:    $legsExercises
+        Упражнения для спины:  $backExercises
+        Упражнения для пресса: $absExercises
     """.trimIndent())
 }
